f5c1c9b79679d61b0c14695a45a1ed652ac08199
StarBeasts <ridleyevan60@yahoo.com>
2024-11-08 07:13:59 -0500

    fixed a broken route connection

--- data/maps/headers/Route7.asm
+++ data/maps/headers/Route7.asm
@@ -1,4 +1,4 @@
 	map_header Route7, ROUTE_7, OVERWORLD, NORTH | WEST
-	connection north, Route8, ROUTE_8, 10
+	connection north, Route8, ROUTE_8, 7
 	connection west, LavenderTown, LAVENDER_TOWN, 4
 	end_map_header
