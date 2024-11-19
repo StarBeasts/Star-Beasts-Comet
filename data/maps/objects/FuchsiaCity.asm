670e938be2bd71b9e7c0e54e8800e296149f3fd7
StarBeasts <ridleyevan60@yahoo.com>
2024-11-19 16:29:43 -0500

    rhinestone early EARLY draft

--- data/maps/objects/FuchsiaCity.asm
+++ data/maps/objects/FuchsiaCity.asm
@@ -2,42 +2,42 @@ FuchsiaCity_Object:
 	db $f ; border block
 
 	def_warp_events
-	warp_event  5, 13, FUCHSIA_MART, 1
-	warp_event 11, 27, FUCHSIA_BILLS_GRANDPAS_HOUSE, 1
+	warp_event 11, 27, FUCHSIA_MART, 1
+	warp_event 11, 17, FUCHSIA_BILLS_GRANDPAS_HOUSE, 1
 	warp_event 19, 27, FUCHSIA_POKECENTER, 1
-	warp_event 27, 27, WARDENS_HOUSE, 1
-	warp_event 18,  3, SAFARI_ZONE_GATE, 1
-	warp_event  5, 27, FUCHSIA_GYM, 1
-	warp_event 22, 13, FUCHSIA_MEETING_ROOM, 1
-	warp_event 31, 27, FUCHSIA_GOOD_ROD_HOUSE, 2
-	warp_event 31, 24, FUCHSIA_GOOD_ROD_HOUSE, 1
+	warp_event 17, 17, WARDENS_HOUSE, 1
+	warp_event 31, 17, SAFARI_ZONE_GATE, 1
+	warp_event  3, 19, GAME_CORNER, 1
+	warp_event 31, 27, FUCHSIA_MEETING_ROOM, 1
+	warp_event 18,  3, ROUTE_6_GATE, 2
+	warp_event 25, 27, ROUTE_12_SUPER_ROD_HOUSE, 1
 
 	def_bg_events
 	bg_event 15, 23, 11 ; FuchsiaCityText11
 	bg_event 25, 15, 12 ; FuchsiaCityText12
 	bg_event 17,  5, 13 ; FuchsiaCityText13
-	bg_event  6, 13, 14 ; MartSignText
+	bg_event 16, 27, 14 ; MartSignText
 	bg_event 20, 27, 15 ; PokeCenterSignText
 	bg_event 27, 29, 16 ; FuchsiaCityText16
 	bg_event 21, 15, 17 ; FuchsiaCityText17
 	bg_event  5, 29, 18 ; FuchsiaCityText18
 	bg_event 33,  7, 19 ; FuchsiaCityText19
-	bg_event 27,  7, 20 ; FuchsiaCityText20
+	bg_event 25,  7, 20 ; FuchsiaCityText20
 	bg_event 13,  7, 21 ; FuchsiaCityText21
-	bg_event 31, 13, 22 ; FuchsiaCityText22
+	bg_event 29, 15, 22 ; FuchsiaCityText22
 	bg_event 13, 15, 23 ; FuchsiaCityText23
 	bg_event  7,  7, 24 ; FuchsiaCityText24
 
 	def_object_events
-	object_event 10, 12, SPRITE_YOUNGSTER, WALK, LEFT_RIGHT, 1 ; person
-	object_event 13, 20, SPRITE_OFFICER, WALK, LEFT_RIGHT, 2 ; person
-	object_event 30, 14, SPRITE_FISHER, STAY, DOWN, 3 ; person
+	object_event 12, 11, SPRITE_YOUNGSTER, WALK, LEFT_RIGHT, 1 ; person
+	object_event 25, 22, SPRITE_OFFICER, WALK, LEFT_RIGHT, 2 ; person
+	object_event 28, 16, SPRITE_FISHER, STAY, DOWN, 3 ; person
 	object_event 25,  8, SPRITE_YOUNGSTER, STAY, UP, 4 ; person
-	object_event 32,  5, SPRITE_MONSTER, STAY, DOWN, 5 ; person
-	object_event 25,  6, SPRITE_MONSTER, STAY, NONE, 6 ; person
-	object_event 12,  6, SPRITE_MONSTER, WALK, LEFT_RIGHT, 7 ; person
-	object_event 30, 12, SPRITE_MONSTER, WALK, LEFT_RIGHT, 8 ; person
-	object_event  8, 17, SPRITE_SEEL, WALK, ANY_DIR, 9 ; person
-	object_event  6,  5, SPRITE_BIRD, STAY, NONE, 10 ; person
+	object_event 30,  5, SPRITE_MONSTER, STAY, DOWN, 5 ; person
+	object_event 24,  6, SPRITE_MONSTER, STAY, NONE, 6 ; person
+	object_event 11,  5, SPRITE_MONSTER, WALK, LEFT_RIGHT, 7 ; person
+	object_event 28, 12, SPRITE_MONSTER, WALK, LEFT_RIGHT, 8 ; person
+	object_event  5,  5, SPRITE_SEEL, STAY, ANY_DIR, 9 ; person
+	object_event 19, 11, SPRITE_BIRD, STAY, NONE, 10 ; person
 
 	def_warps_to FUCHSIA_CITY
