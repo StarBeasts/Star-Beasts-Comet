aa1ac37a79863d1a9ffe05727d2f3479f3b6781a
StarBeasts <ridleyevan60@yahoo.com>
2024-12-02 09:36:34 -0500

    added CUT grass explainer NPC to route 2

--- data/maps/objects/Route2.asm
+++ data/maps/objects/Route2.asm
@@ -9,13 +9,14 @@ Route2_Object:
 	warp_event 12,  25, MT_MOON_POKECENTER, 2
 
 	def_bg_events
-	bg_event 27, 11, 3 ; Route2Text3
-	bg_event  5,  5, 4 ; Route2Text4
+	bg_event 27, 11, 4 ; Route2Text3
+	bg_event  5,  5, 5 ; Route2Text4
 
 	def_object_events
 	object_event 4, 19, SPRITE_POKE_BALL, STAY, NONE, 1, MOON_STONE
 	object_event  4,  4, SPRITE_POKE_BALL, STAY, NONE, 2, HP_UP
-
+	object_event  6,  10, SPRITE_YOUNGSTER, STAY, LEFT, 3
+; grass guy
 	def_warps_to ROUTE_2
 
 	; unused
--- text/Route2.asm
+++ text/Route2.asm
@@ -1,9 +1,28 @@
 _Route2Text3::
+	text "See this right"
+	line "here?"
+
+	para "This here's RELAS"
+	line "TUSSOCK GRASS."
+
+	para "You can't walk"
+	line "through it and"
+	cont "not get scratched"
+	cont "up to heck and"
+	cont "back again!"
+
+	para "No, I reckon you"
+	line "need something"
+	cont "that can CUT it"
+	cont "to get past it."
+	done
+
+_Route2Text4::
 	text "ROUTE 2"
 	line "OLIVINE ORCHARD -"
 	cont "AMETHYST AVENUE"
 	done
 
-_Route2Text4::
+_Route2Text5::
 	text "OLIVINE ORCHARD"
 	done
