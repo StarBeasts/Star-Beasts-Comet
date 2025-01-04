ad26ef65f3afbd654d32b3339163eb4921d277ef
StarBeasts <ridleyevan60@yahoo.com>
2025-01-04 10:15:06 -0500

    miserite meadow draft with more stuff

--- data/maps/objects/RocketHideoutB1F.asm
+++ data/maps/objects/RocketHideoutB1F.asm
@@ -6,17 +6,17 @@ RocketHideoutB1F_Object:
 	warp_event 3,  27, ROUTE_8, 5
 	warp_event  2, 24, ROCKET_HIDEOUT_B1F, 4
 	warp_event 26, 23, ROCKET_HIDEOUT_B1F, 3
-	warp_event 32,  6, ROCKET_HIDEOUT_B4F, 1
+	warp_event 32,  6, ROCKET_HIDEOUT_B2F, 1
 
 	def_bg_events
 
 	def_object_events
-	object_event 22, 18, SPRITE_ROCKET, STAY, LEFT, 1, OPP_ROCKET, 8
-	object_event 20, 14, SPRITE_ROCKET, STAY, RIGHT, 2, OPP_ROCKET, 9
-	object_event 32, 14, SPRITE_ROCKET, STAY, DOWN, 3, OPP_ROCKET, 10
-	object_event 33, 19, SPRITE_ROCKET, STAY, RIGHT, 4, OPP_ROCKET, 11
+	object_event 24, 20, SPRITE_ROCKET, STAY, RIGHT, 1, OPP_ROCKET, 8
+	object_event 32, 14, SPRITE_ROCKET, STAY, DOWN, 2, OPP_ROCKET, 9
+	object_event 17, 14, SPRITE_ROCKET, STAY, UP, 3, OPP_ROCKET, 10
+	object_event 21, 12, SPRITE_ROCKET, STAY, DOWN, 4, OPP_ROCKET, 11
 	object_event 28, 10, SPRITE_ROCKET, STAY, LEFT, 5, OPP_ROCKET, 12
-	object_event  0, 22, SPRITE_POKE_BALL, STAY, NONE, 6, ESCAPE_ROPE
-	object_event 34, 11, SPRITE_POKE_BALL, STAY, NONE, 7, HYPER_POTION
+	object_event 18, 12, SPRITE_POKE_BALL, STAY, NONE, 6, ESCAPE_ROPE
+	object_event 17,  6, SPRITE_POKE_BALL, STAY, NONE, 7, HYPER_POTION
 
 	def_warps_to ROCKET_HIDEOUT_B1F
--- data/maps/objects/RocketHideoutB3F.asm
+++ data/maps/objects/RocketHideoutB3F.asm
@@ -1,16 +1,16 @@
 RocketHideoutB3F_Object:
-	db $2e ; border block
+	db $0F ; border block
 
 	def_warp_events
-	warp_event 25,  6, ROCKET_HIDEOUT_B2F, 2
-	warp_event 19, 18, ROCKET_HIDEOUT_B4F, 1
+	warp_event  6, 21, ROCKET_HIDEOUT_B2F, 2
+	warp_event 16,  7, ROCKET_HIDEOUT_B4F, 1
 
 	def_bg_events
 
 	def_object_events
-	object_event 10, 22, SPRITE_ROCKET, STAY, RIGHT, 1, OPP_ROCKET, 14
-	object_event 26, 12, SPRITE_ROCKET, STAY, UP, 2, OPP_ROCKET, 15
-	object_event 26, 17, SPRITE_POKE_BALL, STAY, NONE, 3, TM_DOUBLE_EDGE
-	object_event 20, 14, SPRITE_POKE_BALL, STAY, NONE, 4, RARE_CANDY
+	object_event  3,  4, SPRITE_ROCKET, STAY, RIGHT, 1, OPP_ROCKET, 14
+	object_event 14,  8, SPRITE_ROCKET, STAY, RIGHT, 2, OPP_ROCKET, 15
+	object_event  5,  2, SPRITE_POKE_BALL, STAY, NONE, 3, TM_DOUBLE_EDGE
+	object_event  4,  2, SPRITE_POKE_BALL, STAY, NONE, 4, RARE_CANDY
 
 	def_warps_to ROCKET_HIDEOUT_B3F
