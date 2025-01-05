baebd5c46be82e3e207e03d8b82813740a9dc091
StarBeasts <ridleyevan60@yahoo.com>
2025-01-05 08:36:01 -0500

    some minor touchups to miserite meadow

--- data/maps/objects/RocketHideoutB3F.asm
+++ data/maps/objects/RocketHideoutB3F.asm
@@ -10,7 +10,7 @@ RocketHideoutB3F_Object:
 	def_object_events
 	object_event  3,  4, SPRITE_ROCKET, STAY, RIGHT, 1, OPP_ROCKET, 14
 	object_event 14,  8, SPRITE_ROCKET, STAY, RIGHT, 2, OPP_ROCKET, 15
-	object_event  5,  2, SPRITE_POKE_BALL, STAY, NONE, 3, TM_DOUBLE_EDGE
-	object_event  4,  2, SPRITE_POKE_BALL, STAY, NONE, 4, RARE_CANDY
+	object_event 28,  3, SPRITE_POKE_BALL, STAY, NONE, 3, TM_DOUBLE_EDGE
+	object_event  5,  2, SPRITE_POKE_BALL, STAY, NONE, 4, RARE_CANDY
 
 	def_warps_to ROCKET_HIDEOUT_B3F
