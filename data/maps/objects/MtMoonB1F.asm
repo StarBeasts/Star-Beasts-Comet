8691c623e07e2b14cc40e2843580f8de9f4116f7
StarBeasts <ridleyevan60@yahoo.com>
2025-01-28 22:43:21 -0500

    finalized Cobalt Cave layout, some tiles have broken collision

--- data/maps/objects/MtMoonB1F.asm
+++ data/maps/objects/MtMoonB1F.asm
@@ -8,8 +8,12 @@ MtMoonB1F_Object:
 	warp_event 17, 35, MT_MOON_1F, 7
 	warp_event 25, 23, MT_MOON_1F, 5
 	warp_event 23, 43, MT_MOON_1F, 6
-	warp_event 17, 15, LAST_MAP, 3
-	warp_event 16, 15, MT_MOON_B2F, 3
+	warp_event 29,  5, MT_MOON_B2F, 2
+	warp_event 15,  5, MT_MOON_B2F, 8
+	warp_event 21, 15, MT_MOON_B2F, 7
+	warp_event  9, 15, MT_MOON_B2F, 3
+	warp_event  5,  5, MT_MOON_B2F, 1
+
 
 	def_bg_events
 
