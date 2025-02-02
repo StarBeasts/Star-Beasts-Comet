cf2c580a0bbed0811141dae9382942c7ca1181c8
StarBeasts <ridleyevan60@yahoo.com>
2025-02-02 16:45:01 -0500

    changed first set of "Star Flute" coords

--- data/maps/headers/Route12.asm
+++ data/maps/headers/Route12.asm
@@ -1,5 +1,3 @@
-	map_header Route12, ROUTE_12, OVERWORLD, NORTH | SOUTH | WEST
-	connection north, LavenderTown, LAVENDER_TOWN, 0
-	connection south, Route13, ROUTE_13, 0
+	map_header Route12, ROUTE_12, OVERWORLD, WEST
 	connection west, Route11, ROUTE_11, 0
 	end_map_header
--- engine/items/item_effects.asm
+++ engine/items/item_effects.asm
@@ -1790,10 +1790,10 @@ WakeUpEntireParty:
 	ret
 
 Route12SnorlaxFluteCoords:
-	dbmapcoord  9, 62 ; one space West of Snorlax
-	dbmapcoord 10, 61 ; one space North of Snorlax
-	dbmapcoord 10, 63 ; one space South of Snorlax
-	dbmapcoord 11, 62 ; one space East of Snorlax
+	dbmapcoord  9, 16 ; one space West of Snorlax
+	dbmapcoord 10, 15 ; one space North of Snorlax
+	dbmapcoord 10, 17 ; one space South of Snorlax
+	dbmapcoord 11, 16 ; one space East of Snorlax
 	db -1 ; end
 
 Route16SnorlaxFluteCoords:
