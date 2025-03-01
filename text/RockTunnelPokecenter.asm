c594c142723fbe0ca50ab3e623f61dc639b77dd8
StarBeasts <ridleyevan60@yahoo.com>
2025-03-01 11:10:04 -0500

    ranger station center and mart stuff?

--- text/RockTunnelPokecenter.asm
+++ text/RockTunnelPokecenter.asm
@@ -40,7 +40,7 @@ _RockTunnelPokecenterText6::
 
 	para "You ask me, only"
 	line "thing special"
-	cont "'bout the cave"
+	cont "about the cave"
 	cont "is that you can't"
 	cont "hardly see but"
 	cont "two steps in"
