923c60cb51ac38ff83240b6c62e8fc80c1e27474
StarBeasts <ridleyevan60@yahoo.com>
2025-04-20 10:28:00 -0400

    mirrored Cracture Chasm so it matches the map

--- data/maps/objects/DiglettsCave.asm
+++ data/maps/objects/DiglettsCave.asm
@@ -2,14 +2,14 @@ DiglettsCave_Object:
 	db $19 ; border block
 
 	def_warp_events
-	warp_event 37, 31, ROUTE_6, 4
-	warp_event 11,  5, PALLET_TOWN, 4
-	warp_event 31,  9, ANCIENT_CAVE, 1
-	warp_event 31, 13, DEVILS_CANYON, 1
+	warp_event  3, 31, ROUTE_6, 4
+	warp_event 29,  5, PALLET_TOWN, 4
+	warp_event  8,  8, ANCIENT_CAVE, 1
+	warp_event  8, 12, DEVILS_CANYON, 1
 
 	def_bg_events
 
 	def_object_events
-	object_event 23,  9, SPRITE_BOULDER, STAY, DOWN, 1
-	object_event 23, 13, SPRITE_BOULDER, STAY, DOWN, 2
+	object_event 16,  9, SPRITE_BOULDER, STAY, DOWN, 1
+	object_event 16, 13, SPRITE_BOULDER, STAY, DOWN, 2
 	def_warps_to DIGLETTS_CAVE
