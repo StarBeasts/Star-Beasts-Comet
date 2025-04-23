22c6aa9f3c8ec8ec6b40677f4ff1f82e7b827a4a
StarBeasts <ridleyevan60@yahoo.com>
2025-04-23 14:59:31 -0400

    Deep Lore of Star Beasts:tm:

--- text/AmethystArchive.asm
+++ text/AmethystArchive.asm
@@ -1,5 +1,14 @@
 _AmethystArchiveText1::
-	text "1."
+	text "Welcome to"
+	line "AMETHYST ARCHIVE!"
+
+	para "We have all sorts"
+	line "of books about"
+	cont "# BEASTS!"
+
+	para "Feel welcome to"
+	line "study them as"
+	cont "you please!"
 	done
 
 _AmethystArchiveText2::
@@ -11,19 +20,190 @@ _AmethystArchiveText3::
 	done
 
 _AmethystArchiveText4::
-	text "4."
+	text "# BEASTS:"
+	line "A HISTORY"
+	cont "BOOK 1"
+
+	para "BY RUCELIF VELID"
+
+	para "Many years ago,"
+	line "an incredibly"
+	cont "massive meteor"
+	cont "struck the EARTH."
+
+	para "But this was no"
+	line "ordinary comet."
+
+	para "It carried with it"
+	line "an otherworldly"
+	cont "substance, in a"
+	cont "state of matter"
+	cont "not known to man."
+
+	para "We would come to"
+	line "call this matter"
+	cont "'Magic.'"
 	done
 
 _AmethystArchiveText5::
-	text "5."
+	text "# BEASTS:"
+	line "A HISTORY"
+	cont "BOOK 2"
+
+	para "When the meteor"
+	line "landed, a strange,"
+	cont "mystical aura"
+	cont "blanketed the"
+	cont "land..."
+
+	para "People began"
+	line "to die or come"
+	cont "down with strange"
+	cont "sicknesses..."
+
+	para "Animals were"
+	line "twisted, warped."
+
+	para "They began to"
+	line "exhibit strange,"
+	cont "unnatural powers."
+
+	para "Not only that,"
+	line "some inanimate"
+	cont "objects sprang"
+	cont "to life, and"
+	cont "began to act"
+	cont "as if they had"
+	cont "always been"
+	cont "alive, magical"
+	cont "powers included."
+
+	para "These creatures"
+	line "would come to be"
+	cont "known as BEASTs."
+
+	para "When we started"
+	line "to have a better"
+	cont "understanding of"
+	cont "the link between"
+	cont "the meteor and"
+	cont "their powers..."
+
+	para "A new name was"
+	line "given to them:"
+
+	para "# BEASTS."
+
 	done
 
 _AmethystArchiveText6::
-	text "6."
+	text "# BEASTS:"
+	line "A HISTORY"
+	cont "BOOK 3"
+
+	para "Each and every"
+	line "animal was gifted"
+	cont "a special sort"
+	cont "of magic."
+
+	para "In modern times,"
+	line "we classify said"
+	cont "magic using the"
+	cont "ELEMENT TYPES"
+	cont "system."
+
+	para "An ELEMENT TYPE"
+	line "is a form of"
+	cont "magic or power"
+	cont "that any given"
+	cont "# BEAST is"
+	cont "naturally adept"
+	cont "at wielding."
+
+	para "Some enhance the"
+	line "innate abilities"
+	cont "of a BEAST, and"
+	cont "others give them"
+	cont "control over"
+	cont "nature, or even"
+	cont "the supernatural."
+
+	para "Even those BEASTs"
+	line "we christened"
+	cont "'NORMAL' type"
+	cont "are only called"
+	cont "that because they"
+	cont "don't specialize"
+	cont "in any particular"
+	cont "field of magic,"
+	cont "instead only"
+	cont "having passable"
+	cont "abilities in many"
+	cont "fields of magic."
+
 	done
 
 _AmethystArchiveText7::
-	text "7."
+	text "# BEASTS:"
+	line "A HISTORY"
+	cont "BOOK 4"
+	
+	para "Oddly, humans"
+	line "were mostly left"
+	cont "unaffected by"
+	cont "the meteor, aside"	
+	cont "from those killed"
+	cont "by the impact and"
+	cont "its resulting"
+	cont "shockwave."
+
+	para "For a while, all"
+	line "we could do was"
+	cont "acclimate to the"
+	cont "new, BEAST-filled"
+	cont "world we found"
+	cont "ourselves in."
+
+	para "And yet..."
+
+	para "New generations"
+	line "would be born,"
+	cont "with PSYCHIC"
+	cont "abilities."
+
+	para "It is my theory"
+	line "that if magical"
+	cont "powers exist in"
+	cont "humans, they"
+	cont "usually manifest"
+	cont "in PSYCHIC form."
+
+	para "However, a scant"
+	line "few humans, such"
+	cont "as myself, are"
+	cont "blessed with"
+	cont "control over"
+	cont "the ELEMENTs,"
+	cont "such as ICE, or"
+	cont "in my case, FIRE."
+
+	para "After much talk"
+	line "in the scientific"
+	cont "community, these"
+	cont "very rare people"
+	cont "would come to be"
+	cont "known as MANCERs."
+
+	para "It is my single"
+	line "greatest ambition"
+	cont "in life to bestow"
+	cont "this magical"
+	cont "prowess on every"
+	cont "man, woman, and"
+	cont "child that I can."
+
+	para "Just think of"
+	line "the possibilities!"
 	done
 
 _AmethystArchiveText8::
