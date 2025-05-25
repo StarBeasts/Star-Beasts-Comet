9764c26dcd6effe2e8287692921476fa0e3593f4
StarBeasts <ridleyevan60@yahoo.com>
2025-05-25 19:00:07 -0400

    halfway done with KUNZITE lab

--- scripts/CinnabarLab.asm
+++ scripts/CinnabarLab.asm
@@ -8,6 +8,22 @@ CinnabarLab_TextPointers:
 	dw Lab1Text3
 	dw Lab1Text4
 	dw Lab1Text5
+	dw Lab1Text6
+	dw Lab1Text7
+	dw Lab1Text8
+	dw Lab1Text9
+	dw Lab1Text10
+	dw Lab1Text11
+	dw Lab1Text12
+	dw Lab1Text13
+	dw Lab1Text14
+	dw Lab1Text15
+	dw Lab1Text16
+	dw Lab1Text17
+	dw Lab1Text18
+	dw Lab1Text19
+	dw Lab1Text20
+	dw Lab1Text21
 
 Lab1Text1:
 	text_far _Lab1Text1
@@ -28,3 +44,69 @@ Lab1Text4:
 Lab1Text5:
 	text_far _Lab1Text5
 	text_end
+
+Lab1Text6:
+	text_far _Lab1Text6
+	text_end
+
+Lab1Text7:
+	text_far _Lab1Text7
+	text_end
+ 
+Lab1Text8:
+	text_far _Lab1Text8
+	text_end
+
+Lab1Text9: 
+ 	text_far _Lab1Text9
+	text_end
+
+Lab1Text10:
+	text_far _Lab1Text10
+	text_end
+
+Lab1Text11:
+	text_far _Lab1Text11
+	text_end
+
+Lab1Text12:
+	text_far _Lab1Text12
+	text_end
+
+Lab1Text13:
+	text_far _Lab1Text13
+	text_end
+
+Lab1Text14:
+	text_far _Lab1Text14
+	text_end
+
+Lab1Text15:
+	text_far _Lab1Text15
+	text_end
+
+Lab1Text16:
+	text_far _Lab1Text16
+	text_end
+
+Lab1Text17:
+	text_far _Lab1Text17
+	text_end
+
+Lab1Text18:
+	text_far _Lab1Text18
+	text_end
+
+Lab1Text19:
+	text_far _Lab1Text19
+	text_end
+
+Lab1Text20:
+	text_far _Lab1Text20
+	text_end
+
+Lab1Text21:
+	text_far _Lab1Text21
+	text_end
+
+
--- text/CinnabarLab.asm
+++ text/CinnabarLab.asm
@@ -8,22 +8,156 @@ _Lab1Text1::
 	done
 
 _Lab1Text2::
+	text "I found this very"
+	line "strange soul in"
+	cont "COBALT CAVE!"
+
+	para "There's a kind of"
+	line "ebb to it, like"
+	cont "the belly of a"
+	cont "pregnant demon..."
+	done
+
+_Lab1Text3::
+	text "Oho! We don't get"
+	line "too many people"
+	cont "passing through"
+	cont "here that don't"
+	cont "work here!"
+
+	para "Welcome! Welcome"
+	line "to KUNZITE KEY!"
+	done
+
+_Lab1Text4::
+	text "My DRAGLEAM is a"
+	line "different color"
+	cont "than most of them"
+	cont "are. I brought it"
+	cont "here to see what"
+	cont "the research team"
+	cont "thinks of it."
+	done
+
+_Lab1Text5::
+	text "We study BEASTS"
+	line "extensively here."
+
+	para "People often bring"
+	line "us rare BEASTS"
+	cont "for examination."
+	done
+
+_Lab1Text6::
+	text "We study BEASTS"
+	line "extensively here."
+
+	para "People often bring"
+	line "us rare BEASTS"
+	cont "for examination."
+	done
+
+_Lab1Text7::
+	text "We study BEASTS"
+	line "extensively here."
+
+	para "People often bring"
+	line "us rare BEASTS"
+	cont "for examination."
+	done
+
+_Lab1Text8::
+	text "Welcome to"	
+	line "KUNZITE KEY."
+
+	para "If you're clocking"
+	line "in, you'll need"
+	cont "to either log in"
+	cont "or sign on the"
+	cont "sheet here."
+
+	para "If you're just"
+	line "passing by, don't"
+	cont "bother the"
+	cont "research team,"
+	cont "please."
+
+	para "Enjoy your visit."
+	done
+
+_Lab1Text9::
+	text "Bit of a trek"
+	line "to get here,"
+	cont "ain't it?"
+
+	para "Aye, but there's"
+	line "nothin' like"
+	cont "seein' the SEA"
+	cont "ta fill ya with"
+	cont "glee! Haharr!"
+	done
+
+_Lab1Text10::
+	text "Aaah! Hey!"
+	
+	para "What? What am I"
+	line "doing?"
+
+	para "Well, what's it"
+	line "look like?"
+
+	para "Playing video"
+	line "games?"
+	
+	para "N-no, I'm not!"
+
+	para "I'm just..."
+
+	para "Running some"
+	line "tests!"
+
+	para "Yes! Vital tests!"
+	done
+
+_Lab1Text11::
+	text "If you're looking"
+	line "for the SOUL"
+	cont "RESURRECTION"
+	cont "FACILITY, it's"
+	cont "not here."
+
+	para "They decided they"
+	line "were TOO GOOD for"
+	cont "us, and relocated"
+	cont "to EMERALD ACRES,"
+	cont "east of AMETHYST"
+	cont "AVENUE."
+
+	para "Hmph!"
+
+	para "Good riddance to"
+	line "bad rubbish,"
+	cont "you ask me!"
+	
+	done
+
+_Lab1Text12::
 	text "A photo of the"
 	line "LAB's founder,"
 	cont "DR.EVEREST!"
 	done
 
-_Lab1Text3::
+_Lab1Text13::
 	text "# BEAST LAB"
 	line "Meeting Room"
 	done
 
-_Lab1Text4::
+_Lab1Text14::
 	text "# BEAST LAB"
 	line "R-and-D Room"
 	done
 
-_Lab1Text5::
+_Lab1Text15::
 	text "# BEAST LAB"
 	line "Computer Room"
 
@@ -36,3 +170,40 @@ _Lab1Text5::
 	cont "RELOCATED TO"
 	cont "EMERALD ACRES"
 	done
+
+_Lab1Text16::
+	text "It needs a"
+	line "password."
+	done
+
+_Lab1Text17::
+	text "Hey!"
+
+	para "This guy is"
+	line "running #"
+	cont "BEASTS COMET"
+	cont "VERSION on an"
+	cont "emulator!"
+	done
+
+_Lab1Text18::
+	text "EXIT TO"
+	line "ROUTE 11"
+	done
+
+_Lab1Text19::
+	text "KUNZITE KEY"
+	done
+
+_Lab1Text20::
+	text "SEA ROUTE TO"
+	line "SODALITE SINKHOLE"
+	done
+
+_Lab1Text21::
+	text "They appear to"
+	line "pipes used for"
+	cont "smoking."
+	done
+
+
