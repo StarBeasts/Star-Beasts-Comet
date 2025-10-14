164b1bfef75b1986a47eff68a8270eb35d468be0
StarBeasts <ridleyevan60@yahoo.com>
2025-10-14 19:46:18 -0400

    fixed robot overworld sprite and gave first shuttle robot dialogue

--- text/RocketHideoutElevator.asm
+++ text/RocketHideoutElevator.asm
@@ -1,6 +1,41 @@
 _RocketHideoutElevatorText1::
-	text "I TALK ABOUT"
-	line "SHIP FOR YOU!"
+	text "WELCOME USER"
+
+	para "THIS IS GAI CO.'S"
+	line "STATE OF THE ART"
+	cont "INTERSTELLAR"
+	cont "TRAVEL SHUTTLE"
+
+	para "TO NAVIGATE,"
+	line "INTERACT WITH THE"
+	cont "CONTROL PANEL NEXT"
+	cont "TO THE DOORS"
+
+	para "THE SHIP HAS AN"
+	line "ATMOSPHERIC FIELD"
+	cont "GENERATOR"
+
+	para "IF YOU ARE OUT OF"
+	line "RANGE, IT WILL"
+	cont "STOP YOU BY"
+
+	para "-BZZT!-"
+
+	para "IT IS ALSO"
+	line "STOCKED WITH OUR"
+	cont "ROBOVENDOR, WHO"
+	cont "SELLS ITEMS TO"
+	cont "YOU AT AN"
+	cont "AFFORDABLE PRICE,"
+	cont "AND OUR NEW"
+	cont "CUTTING-EDGE"
+	cont "HEALERBOT,"
+	cont "BOTH DIRECTLY"
+	cont "NEXT TO ME."
+
+	para "WE HOPE YOU ENJOY"
+	line "YOUR FLIGHT,"
+	cont "'USER NAME HERE.'"
 	prompt
 
 _RocketHideoutElevatorText2::
